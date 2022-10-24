488177359120cb9d0dfc792928c26e271257a91a
ShimkovaVV <109003039+ShimkovaVV@users.noreply.github.com>
2022-10-24 23:52:05 +0300

    Add files via upload

---
+++ JS_1hm/script/script.js
@@ -0,0 +1,17 @@
+let userAge = 12;
+let userName = 'Viktoriia';
+let userSurname = 'Pinchuk';
+console.log(userAge);
+console.log(userName);
+console.log(userSurname);
+
+// userNew;
+// shoppingList;
+// userOnline;
+// staffSalary;
+// dataCursor;
+
+console.log('Hello World!');
+
+let userNew = 'Shimkova';
+console.log('userNew', userNew)
